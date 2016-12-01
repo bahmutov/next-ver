4a5f6143f92ca75fbd74bc206944496b0fe0466f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-12-01 17:51:46 -0500

    more work

--- bin/next-ver.js
+++ bin/next-ver.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
 
-console.log('next-ver')
+const join = require('path').join
+const cwd = process.cwd()
+const pkg = require(join(cwd, 'package.json'))
+
+console.log('next-ver after', pkg.version)
+
+const ggit = require('ggit')
+ggit.commits.all(cwd)
+  .then(console.log)
+  .done()
