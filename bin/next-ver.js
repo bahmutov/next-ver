8764be97d6ae468560436eea60f48ab04425c1d2
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-12-02 00:42:35 -0500

    fetching all commit messages after the last version tag

--- bin/next-ver.js
+++ bin/next-ver.js
@@ -3,10 +3,12 @@
 const join = require('path').join
 const cwd = process.cwd()
 const pkg = require(join(cwd, 'package.json'))
+const R = require('ramda')
 
 console.log('next-ver after', pkg.version)
 
 const ggit = require('ggit')
-ggit.commits.all(cwd)
+ggit.commits.afterLastTag()
+  .then(R.map(R.prop('message')))
   .then(console.log)
   .done()
